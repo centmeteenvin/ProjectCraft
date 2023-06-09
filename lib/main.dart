3bfcb19e6c39cde280cfba28ba0b10b2e35f631f
vincent verbergt <vincent.verbergt@gmail.com>
2023-06-09 14:33:55 +0200

    Initialized Firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: "../.env");
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.web,
+  );
   runApp(const MainApp());
 }
 
