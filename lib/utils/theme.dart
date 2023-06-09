9bf4d57ed9eb769b4a95fd682623272195b6c524
vincent verbergt <vincent.verbergt@gmail.com>
2023-06-09 17:41:16 +0200

    Added a theme

---
+++ lib/utils/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+final ThemeData theme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: const Color(0x00673AB7),
+
+
+  fontFamily: 'Roboto',
+
+  textTheme: const TextTheme(
+    titleLarge:  TextStyle(
+      fontSize: 70,
+      fontWeight: FontWeight.bold
+    ), 
+  ),
+
+
+);
