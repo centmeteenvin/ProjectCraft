c869f32c51c734f6827e6db9a0e5aaeb150bcc4c
vincent verbergt <vincent.verbergt@gmail.com>
2023-06-17 17:18:33 +0200

    CopyWith and equals + hashCode for Task

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,14 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:project_craft/models/person.dart';
 
-enum Status {
-  toPlan,
-  planned,
-  started,
-  completed,
-  delayed
-}
+enum Status { toPlan, planned, started, completed, delayed }
 
 @immutable
 class Task {
@@ -33,20 +26,62 @@ class Task {
       required this.status,
       required this.uuid});
 
-  
   ///Converts the current object to a Map with string keys.
   ///All fields that are of type Person, Project or Task, are mapped to their corresponding uuid.
-  Map<String,dynamic> toMap() {
+  Map<String, dynamic> toMap() {
     return {
-      "uuid":uuid,
-      "title":title,
-      "description":description,
-      "startDate":startDate,
-      "deadline":deadline,
-      "status":status,
-      "agents":agentIds.toList(),
-      "subTasks":subTaskIds,
-      "dependingOn":dependingOnTaskIds,
+      "uuid": uuid,
+      "title": title,
+      "description": description,
+      "startDate": startDate,
+      "deadline": deadline,
+      "status": status,
+      "agents": agentIds.toList(),
+      "subTasks": subTaskIds,
+      "dependingOn": dependingOnTaskIds,
     };
-  }   
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Task &&
+          runtimeType == other.runtimeType &&
+          uuid == other.uuid &&
+          title == other.title &&
+          description == other.description &&
+          agentIds  == other.agentIds &&
+          subTaskIds == other.subTaskIds &&
+          dependingOnTaskIds == other.dependingOnTaskIds &&
+          startDate == other.startDate &&
+          deadline == other.deadline &&
+          status == other.status;
+
+  @override
+  int get hashCode =>
+    Object.hash(uuid, title, description, agentIds, subTaskIds, dependingOnTaskIds, startDate, deadline, status);
+
+  Task copyWith({
+    String? uuid,
+    String? title,
+    String? description,
+    Set<String>? agentIds,
+    List<String>? subTaskIds,
+    List<String>? dependingOnTaskIds,
+    DateTime? startDate,
+    DateTime? deadline,
+    Status? status,
+  }) {
+    return Task(
+      uuid: uuid ?? this.uuid,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      agentIds: agentIds ?? this.agentIds,
+      subTaskIds: subTaskIds ?? this.subTaskIds,
+      dependingOnTaskIds: dependingOnTaskIds ?? this.dependingOnTaskIds,
+      startDate: startDate ?? this.startDate,
+      deadline: deadline ?? this.deadline,
+      status: status ?? this.status,
+    );
+  }
 }
--- test/models/task_test.dart
+++ test/models/task_test.dart
@@ -79,4 +79,48 @@ void main() {
       }
     });
   });
+  group('CopyWith tests', () {
+    test('no fields changed', () {
+      Task random = randomTask();
+      Task copy = random.copyWith();
+      expect(random, copy);
+    });
+    test('uuid changed', () {
+      Task task1 = randomTask();
+      Task task2 = task1.copyWith(uuid: "test");
+      expect(task1 == task2, false);
+      expect(task2.uuid, "test");
+    });
+  });
+}
+
+Task randomTask() {
+        var agents = <String>{};
+        int amount = Random().nextInt(20);
+        for (int i = 0; i < amount; i++) {
+          agents.add(const Uuid().v4());
+        }
+        var subTasks = <String>[];
+        amount = Random().nextInt(20);
+        for (int i = 0; i < amount; i++) {
+          subTasks.add(const Uuid().v4());
+        }
+        var dependents = <String>[];
+        amount = Random().nextInt(20);
+        for (int i = 0; i < amount; i++) {
+          dependents.add(const Uuid().v4());
+        }
+
+        DateTime now = DateTime.now();
+        return Task(
+          uuid: "1",
+          title: "test",
+          description: "test description",
+          startDate: now,
+          deadline: now,
+          agentIds: agents,
+          subTaskIds: subTasks,
+          dependingOnTaskIds: dependents,
+          status: Status.toPlan,
+        );
 }
