bb6c6fbbb864ba2b95dfacead9da85f7509d15a0
vincent verbergt <vincent.verbergt@gmail.com>
2023-06-17 15:53:53 +0200

    CopyWith and equals + hashCode for Person

--- lib/models/person.dart
+++ lib/models/person.dart
@@ -30,12 +30,49 @@ class Person {
   ///Converts the current object to a Map with string keys.
   ///All fields that are of type Person, Project or Task, are mapped to their corresponding uuid.
   static Person fromMap(Map<String, dynamic> map) {
+    //TODO Unit test.
     return Person(
       uuid:map["uuid"],
       firstName:map["firstName"],
       lastName:map["lastName"],
       photoUrl:map["photoUrl"],
       projectIds:map["projects"],
+    );
+  }
+
+Person copyWith(
+      {String? uuid,
+      String? firstName,
+      String? lastName,
+      String? photoUrl,
+      Set<String>? projectIds}) {
+    return Person(
+      uuid: uuid ?? this.uuid,
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      photoUrl: photoUrl ?? this.photoUrl,
+      projectIds: projectIds ?? this.projectIds,
+    );
+  }
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is Person &&
+        firstName == other.firstName &&
+        lastName == other.lastName &&
+        photoUrl == other.photoUrl &&
+        uuid == other.uuid &&
+        projectIds == other.projectIds;
+  }
+
+  @override
+  int get hashCode {
+    return Object.hash(
+      firstName,
+      lastName,
+      photoUrl,
+      uuid,
+      projectIds,
     );
   }
 }
--- test/models/person_test.dart
+++ test/models/person_test.dart
@@ -22,16 +22,7 @@ void main() {
 
     test('toMap with 3 random projects', () {
       for (int n = 0; n < 100; n++) {
-        var projectIds = <String>{};
-        for (var i = 0; i < 3; i++) {
-          projectIds.add(i.toString());
-        }
-        Person vincent = Person(
-            uuid: "1",
-            firstName: "Vincent",
-            lastName: "Verbergt",
-            photoUrl: "",
-            projectIds: projectIds);
+        Person vincent = randomPerson();
         var resultMap = {
           "uuid": "1",
           "firstName": "Vincent",
@@ -43,4 +34,37 @@ void main() {
       }
     });
   });
+  group('copyWith Tests', () {
+    test('no fields changed', () {
+      Person vincent = randomPerson();
+      Person vincent2 = vincent.copyWith();
+      expect(vincent2, vincent);
+    });
+    test('name Changed', () {
+      Person vincent = randomPerson();
+      Person luna = vincent.copyWith(firstName: "Luna Mei", lastName: "Baeyens");
+      expect(luna.firstName, "Luna Mei");
+      expect(luna.lastName, "Baeyens");
+    });
+
+    test('projects changed', () {
+      Person random = randomPerson();
+      Person other = random.copyWith(projectIds: {"1", "2"});
+      expect(other.projectIds, {"1", "2"});
+      
+    });
+  });
+}
+
+Person randomPerson() {
+        var projectIds = <String>{};
+        for (var i = 0; i < 3; i++) {
+          projectIds.add(i.toString());
+        }
+        return Person(
+            uuid: "1",
+            firstName: "Vincent",
+            lastName: "Verbergt",
+            photoUrl: "",
+            projectIds: projectIds);
 }
