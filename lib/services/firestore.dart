01910658e4eebd664aeaa31714fe434c82f1aa8c
vincent verbergt <vincent.verbergt@gmail.com>
2023-06-23 22:31:46 +0200

    Added the locking logic to the FirestoreService
    Used the atomic transaction model for this

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 // ignore: depend_on_referenced_packages
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:project_craft/utils/exceptions.dart';
 class FireStoreService {
 
   late final FirebaseFirestore _db;
@@ -17,7 +18,7 @@ class FireStoreService {
     return snapshot.exists;
   }
 
-  ///Returns true if a new document was created.
+  ///Returns true if a new document was created. Doesn't overwrite an existing document.
   Future<bool> addDocument(String collection, String docId, Map<String, dynamic> data) async {
     if (await _exists(collection, docId)) return false;
     await  _db.collection(collection).doc(docId).set(data);
@@ -26,20 +27,59 @@ class FireStoreService {
 
 
   ///Returns true if a document was delete.
-  Future<bool> deleteDocument(String collection, String docId) async {
-    if (!await _exists(collection, docId)) return false;
-    await _db.collection(collection).doc(docId).delete();
-    return true;
+  Future<bool> deleteDocument(String collection, String docId, {String? agent}) async {
+    final docref = _db.collection(collection).doc(docId);
+    return await _db.runTransaction<bool>((transaction) async {
+      var snapshot = await transaction.get(docref);
+      if (!snapshot.exists) {
+        return false;
+      }
+      var data = snapshot.data()!;
+      if (!data.containsKey("isLocked")) {
+        transaction.delete(docref);
+        return true;
+      }
+      if (agent == null) {
+        throw NoAgentException(data, data["isLocked"]);
+      }
+      if (snapshot["isLocked"] == false) {
+        throw NotLockedException(data);
+      }
+      if (snapshot["lockedBy"] != agent) {
+        throw IsLockedException(data);
+      }
+      transaction.delete(docref);
+      return true;
+    });
   }
 
-  ///Return true if a new document was created.
-  Future<bool> updateDocument(String collection, String docId, Map<String, dynamic> data) async {
-    if (await _exists(collection, docId)) {
-    await _db.collection(collection).doc(docId).update(data);
-    return false;
-    }
-    await addDocument(collection, docId, data);
-    return true;
+  ///updates a given document, if the document didn't already exist a new one will be created and returning true,
+  ///otherwise return false
+  Future<bool> updateDocument(String collection, String docId, Map<String, dynamic> data, {String? agent}) async {
+    final docref = _db.collection(collection).doc(docId);
+    return await _db.runTransaction<bool>((transaction) async {
+      var snapshot = await transaction.get(docref);
+      if (!snapshot.exists) {
+        transaction.set(docref, data);
+        return true;
+      }
+      var snapshotData = snapshot.data()!;
+      if (!snapshotData.containsKey("isLocked")) {
+        transaction.update(docref, data);
+        return false;
+      }
+      if (agent == null) {
+        throw NoAgentException(snapshotData, snapshotData["isLocked"]);
+      }
+      if (snapshot["isLocked"] == false) {
+        throw NotLockedException(snapshotData);
+      }
+      if (snapshot["lockedBy"] != agent) {
+        throw IsLockedException(snapshotData);
+      }
+      transaction.update(docref, data);
+      return false;
+    });
   }
 
 Future<Map<String,dynamic>?> fetchDocument(String collection, String docId) async {
